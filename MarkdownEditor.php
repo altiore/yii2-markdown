5fe9976dc57bb84854ee9e5bcb0007c51563f14a
Razzwan <razvanlomov@gmail.com>
2016-06-27 06:28:54 +0300

    add require to composer.json

---
+++ MarkdownEditor.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: r
+ * Date: 27.06.16
+ * Time: 6:23
+ */
+
+namespace altiore\yii2\markdown;
+
+use yii\widgets\InputWidget;
+
+class MarkdownEditor extends InputWidget
+{
+
+}
